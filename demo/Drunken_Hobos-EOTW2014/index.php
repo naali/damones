b256cab50f92f4472862b8c157ff0abba7e5f8af
Antti Svenn <antti.svenn@gmail.com>
2014-06-20 16:17:37 +0300

    Added Drunken Hobos

---
+++ demo/Drunken_Hobos-EOTW2014/index.php
@@ -0,0 +1,18 @@
+<?php
+	$debug = true;
+	$length_in_sec = 220;
+	$demo_song = "tune2";
+	
+	$demo_name = "Drunken Hobos by Damones";
+	$demo_description = "Drunken Hobos  by Damones – Released at End of the World 2014";
+
+	$part_dir = '/parts/';
+
+	$demo_part_order = array(
+		$part_dir.'part-00-start.js'
+	);
+	
+	
+
+	require_once("../../engine_base.php");
+?>
