fda04aef4536f9a6b52677d3f20fe7acdad989ad
Antti Svenn <antti.svenn@gmail.com>
2015-07-06 17:37:46 +0300

    Added (almost) final songs

--- demo/Boozembly_invitation_2015/index.php
+++ demo/Boozembly_invitation_2015/index.php
@@ -1,11 +1,11 @@
 <?php
 	$debug = true;
         $showcontrols = true;
-	$length_in_sec = 220;
-	$demo_song = "Kikka";
+	$length_in_sec = 328; // 5min 28sec
+	$demo_song = "kikka_tahdon_viihdyttaa_cover_wal";
 	
-	$demo_name = "Boozembly invitation 2015 by Damones";
-	$demo_description = "Boozembly invitation 2015 by Damones – Released at ??????????????. 21st Boozembly will be disorganized during 30th of July - 2nd of August 2015.";
+	$demo_name = "Boozembly 2015 invitation by Damones";
+	$demo_description = "Boozembly 2015 invitation by Damones, Released at Solskogen 2015. 21st Boozembly will be disorganized during 30th of July - 2nd of August 2015 in Helsinki, Finland.";
 
 	$part_dir = '/parts/';
 
